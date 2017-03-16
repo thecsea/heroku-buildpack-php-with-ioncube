14ab6884ce6eb1aabdd1a341b4da082d9d00cb2d
Claudio Cardinale <cardinale.claudio@gmail.com>
2017-03-16 23:03:04 +0100

    install fixed+

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -2,9 +2,10 @@
 
 install_ioncube_ext() {
   if [[ ( ${#exts[@]} -eq 0 || ! ${exts[*]} =~ "ioncube" ) ]]; then
-    install_ext "ioncube" "automatic" "http://heroku-buildpack-php-with-ioncube.s3.amazonaws.com/ioncube.tar.gz"
-    exts+=("ioncube")
-
+    cd /tmp
+    curl http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+    tar xvfz ioncube_loaders_lin_x86-64.tar.gz
+    cp ioncube/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR
     ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
   fi
 }

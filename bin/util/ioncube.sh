73689e22f88b1b268d469b2cb2bbc6d2f2473138
Claudio Cardinale <cardinale.claudio@gmail.com>
2017-03-16 23:21:25 +0100

    install fixed

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -1,6 +1,26 @@
 #!/usr/bin/env bash
 
 install_ioncube_ext() {
+  local series = 
+  case ${ZEND_MODULE_API_VERSION} in
+  	20121212)
+  		series=5.5
+  		;;
+  	20131226)
+  		series=5.6
+  		;;
+  	20151012)
+  		series=7.0
+  		;;
+  	20160303)
+  		series=7.1
+  		;;
+  	*)
+  		echo "Unsupported PHP/Zend Module API version: ${ZEND_MODULE_API_VERSION}"
+  		exit 1
+  		;;
+  esac
+
   cd /tmp
   wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
   tar xvfz ioncube_loaders_lin_x86-64.tar.gz

2eefe59ab84d4fc79409c881e215ae03c34b6582
Claudio Cardinale <cardinale.claudio@gmail.com>
2017-03-16 23:12:05 +0100

    install fixed

--- bin/util/ioncube.sh
+++ bin/util/ioncube.sh
@@ -2,7 +2,7 @@
 
 install_ioncube_ext() {
   cd /tmp
-  curl http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+  wget http://downloads3.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
   tar xvfz ioncube_loaders_lin_x86-64.tar.gz
   cp ioncube/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR
   ln -s $PHP_EXT_DIR/ioncube_loader_lin_${PHP_VERSION%.*}.so $PHP_EXT_DIR/ioncube.so
